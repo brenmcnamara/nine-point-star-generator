51f18099a2645f645b5546cf441b4eb62a7d5e43
Brendan McNamara <brendan.d.mcnamara@vanderbilt.edu>
2018-12-31 15:29:44 -0800

    Adds feature for editing rotation of star

--- src/App.react.js
+++ src/App.react.js
@@ -15,7 +15,21 @@ const H_HEIGHT = HEIGHT / 2;
 const H_WIDTH = WIDTH / 2;
 
 export default class App extends React.Component<Props, State> {
+  _pathRef: * = React.createRef();
+  _rotationRadians: number = 0;
+
   state = {};
+
+  _onChangeRotation = (event: *): void => {
+    const { value } = event.target;
+    const degrees = parseInt(value, 10);
+    if (Number.isNaN(degrees)) {
+      return;
+    }
+
+    this._rotationRadians = degToRad(degrees);
+    this._updateStar();
+  };
 
   render() {
     return (
@@ -28,20 +42,70 @@ export default class App extends React.Component<Props, State> {
               height: `${HEIGHT}px`,
               width: `${WIDTH}px`,
             }}>
-            <path d={this._calculatePath()} fill="transparent" stroke="blue" />
+            <path
+              d={this._calculatePath()}
+              fill="transparent"
+              ref={this._pathRef}
+              stroke="blue"
+            />
           </svg>
         </div>
         <div
-          style={{ backgroundColor: '#F0F0F0', flexGrow: 1, width: '100%' }}
-        />
+          style={{
+            backgroundColor: '#F0F0F0',
+            display: 'flex',
+            flexDirection: 'row',
+            flexGrow: 1,
+            width: '100%',
+          }}>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              flexGrow: 1,
+              padding: '0 8px',
+            }}>
+            <div style={{ margin: '12px 0' }}>
+              <div style={{ marginBottom: '4px' }}>
+                <label>{'Rotation (degrees):'}</label>
+              </div>
+              <div>
+                <input
+                  defaultValue="0"
+                  onChange={this._onChangeRotation}
+                  style={{
+                    borderRadius: '4px',
+                    border: 'solid #AAA 1px',
+                    padding: '2px',
+                    width: '40px',
+                  }}
+                  type="text"
+                />
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              flexGrow: 1,
+              padding: '0 8px',
+            }}
+          />
+        </div>
       </div>
     );
   }
 
   _calculatePath(): string {
     const points = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    const angles = points.map(i => (i * Math.PI * 2) / 9);
-    const midAngles = points.map(i => (Math.PI / 9) * (2 * i + 1));
+
+    const angles = points.map(
+      i => (i * Math.PI * 2) / 9 + this._rotationRadians,
+    );
+    const midAngles = points.map(
+      i => (Math.PI / 9) * (2 * i + 1) + this._rotationRadians,
+    );
 
     const center = { x: H_WIDTH, y: H_HEIGHT };
     const radius = Math.min(H_WIDTH, H_HEIGHT);
@@ -64,4 +128,16 @@ export default class App extends React.Component<Props, State> {
       return `${path} ${part1} ${part2}`;
     }, `M ${starPoints[0].x} ${starPoints[0].y}`);
   }
+
+  _updateStar(): void {
+    this._pathRef.current.setAttribute('d', this._calculatePath());
+  }
+}
+
+function radToDeg(rad: number): number {
+  return (rad / Math.PI) * 180;
+}
+
+function degToRad(deg: number): number {
+  return (deg / 180) * Math.PI;
 }
